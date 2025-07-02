8a7b8561d874ffd2dd94d5149ff68a3dc377909c
Ajaykumar6128 <132042270+Ajaykumar6128@users.noreply.github.com>
2025-07-02 14:53:14 +0530

    Update Problem-1.py

--- Problem-1.py
+++ Problem-1.py
@@ -1,1 +1,33 @@
+class Calculator:
+    print("Calculator")
+    print("Available operations: add, sub, mul, div")
+    def __init__(self, a: float, b: float, operation: str):
+        self.a = a
+        self.b = b
+        self.operation = operation.lower()
+
+    def calculate(self):
+        if self.operation == "add":
+            return self.a + self.b
+        elif self.operation == "sub":
+            return self.a - self.b
+        elif self.operation == "mul":
+            return self.a * self.b
+        elif self.operation == "div":
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Error: Division by zero"
+        else:
+            return "Invalid operation"
+
+
+a = float(input("Enter (a) value: "))
+b = float(input("Enter  (b) value: "))
+operation = input("Enter operation (add, sub, mul, div): ")
+
+calc = Calculator(a, b, operation)
+result = calc.calculate()
+print("Result:", result)
+
 
