8f35e200f3629a615c06f0e716d9d2ef530789cb
Ajaykumar6128 <132042270+Ajaykumar6128@users.noreply.github.com>
2025-07-02 15:18:28 +0530

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,1 +1,17 @@
+def odd_numbers(a):
+    
+    if a % 2 == 0:
+        modify_odd = a - 1
+    else:
+        modify_odd = a
+
+    result = []
+    for i in range(modify_odd):
+        result.append(2 * i + 1)
+
+    return result
+
+a = int(input("Enter (a) value: "))
+output = odd_numbers(a)
+print("Output:", ', '.join(map(str, output)))
 
