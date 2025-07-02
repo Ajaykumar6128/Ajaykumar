28ed440afe03fff36e9128fa6d5066499447b9a6
Ajaykumar6128 <132042270+Ajaykumar6128@users.noreply.github.com>
2025-07-02 15:27:13 +0530

    Update Problem-4.py

--- Problem-4.py
+++ Problem-4.py
@@ -1,1 +1,14 @@
+l=[1,2,3,4,5,6,7,8,9]
+a = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
+result = {}
+
+for i in l:
+    count = 0
+    for j in a:
+        if j % i == 0:
+            count = count+1
+    result[i] = count
+
+
+print(result)
 
